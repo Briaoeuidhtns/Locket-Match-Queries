1023af1d6d4096d94bfcddb6a2192ec0003a7a19
Brian Brunner <brian.brunner.bb@gmail.com>
2019-11-14 13:00:57 -0700

    Add leiningen template project

---
+++ test/locket_match_queries/core_test.clj
@@ -0,0 +1,7 @@
+(ns locket-match-queries.core-test
+  (:require [clojure.test :refer :all]
+            [locket-match-queries.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
