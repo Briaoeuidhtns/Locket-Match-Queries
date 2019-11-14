7df6aa3f3fdd6cb27f1771733f6ae74f9a81a1e4
Brian Brunner <brian.brunner.bb@gmail.com>
2019-11-14 16:07:21 -0700

    Add http lib

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.layerware/hugsql "0.5.1"]
-                 [mysql/mysql-connector-java "8.0.18"]]
+                 [mysql/mysql-connector-java "8.0.18"]
+                 [http.async.client "1.3.1"]]
   :main ^:skip-aot locket-match-queries.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
