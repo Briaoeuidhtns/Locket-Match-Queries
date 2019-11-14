930e9d9f6562847f246c6f782ea4114413e9a334
Brian Brunner <brian.brunner.bb@gmail.com>
2019-11-14 16:05:34 -0700

    Add sql libraries

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/matthewReff/Locket-Match-Queries"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.layerware/hugsql "0.5.1"]
+                 [mysql/mysql-connector-java "8.0.18"]]
   :main ^:skip-aot locket-match-queries.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
