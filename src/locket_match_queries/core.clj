30c96609cb0afbeef437242ae21ec168c950d327
Matthre <geniusammyralltheothernamesare@gmail.com>
2019-12-03 12:51:32 -0700

    Added to recent-matches and team-recent-matches

--- src/locket_match_queries/core.clj
+++ src/locket_match_queries/core.clj
@@ -24,17 +24,29 @@
        (string/replace #"(?<!^)[A-Z]" (comp (partial str \-) (memfn toLowerCase)))
        (string/replace #"[_]|\W+" "-"))))
 
+
+
 (defn recent-matches
   "Get recent matches by player id"
   ([]
    (recent-matches nil))
 
   ([id]
-   (let [url (mkurl "IDOTA2Match_205790/GetMatchHistory/v1")
+   (let [url (mkurl "IDOTA2Match_570/GetMatchHistory/v1")
          response (http/get url {:as :json
                                  :query-params {:key (config :key)
                                                 :account_id id}})]
-     (identity response))))
+    (let [matchList (get-in response [:body :result :matches])] 
+    		(map :match_id matchList)
+    ))
+  )
+ )
+
+(defn team-recent-matches
+	[& team-members]
+						;;(map (comp (partial into #{}) recent-matches) team-members) 
+						(into #{} (flatten (map recent-matches team-members))) 
+)
 
 (def heroes (memo/ttl (fn
                         []
@@ -46,14 +58,15 @@
                           (into {} (map (juxt :id #(as-> % _
                                                      (:name _)
                                                      (re-matches #"(npc_dota_hero)_(.*)" _)
-                                                     (rest _)
+                                                     (rest _) 
                                                      (map proper-keyword _)
                                                      (apply keyword _)))
-                                        hero-list))))
+                                        	hero-list))))
                       :ttl/threshold (-> 24 Duration/ofHours .toMillis)))
 
 (defn -main
   [& args]
   (let [{key :key
          account-id :account_id} config]
-    (pprint (heroes))))
+     (pprint (team-recent-matches 86383285 81729036 179132873))
+     ))
