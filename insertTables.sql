71d6fd00cff3b431f9b2dcf48aa5a79715a214d9
Matthew Reff <geniusammyralltheothernamesare@gmail.com>
2020-05-19 13:01:02 -0600

    Added more data, fixed some columns and applied naming standards to db

--- insertTables.sql
+++ insertTables.sql
@@ -15,122 +15,136 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`MatchTable`
+-- Table `mydb`.`match_table`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`MatchTable` (
-  `matchID` INT NOT NULL,
-  `radiantWin` TINYINT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`match_table` (
+  `match_id` BIGINT(20) NOT NULL,
+  `radiant_win` TINYINT NOT NULL,
   `duration` INT NOT NULL,
-  `towerStatusDire` INT NOT NULL,
-  `towerStatusRadiant` INT NOT NULL,
-  `barracksStatusDire` INT NOT NULL,
-  `barracksStatusRadiant` INT NOT NULL,
-  `firstBloodTime` INT NOT NULL,
-  `radiantScore` INT NOT NULL,
-  `direScore` INT NOT NULL,
-  `pickBan` INT NOT NULL,
-  PRIMARY KEY (`matchID`))
+  `tower_status_dire` INT NOT NULL,
+  `tower_status_radiant` INT NOT NULL,
+  `barracks_status_dire` INT NOT NULL,
+  `barracks_status_radiant` INT NOT NULL,
+  `first_blood_time` INT NOT NULL,
+  `radiant_score` INT NOT NULL,
+  `dire_score` INT NOT NULL,
+  `pick_ban_id` INT NOT NULL,
+  PRIMARY KEY (`match_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`LobbyInfo`
+-- Table `mydb`.`lobby_info`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`LobbyInfo` (
+CREATE TABLE IF NOT EXISTS `mydb`.`lobby_info` (
   `server` VARCHAR(45) NOT NULL,
-  `leagueID` INT NULL,
-  `Season` INT NULL,
-  `startTime` INT NOT NULL,
-  `lobbyType` INT NULL,
-  `matchID` INT NOT NULL,
-  INDEX `fk_LobbyInfo_Match_idx` (`matchID` ASC),
-  PRIMARY KEY (`matchID`),
+  `league_id` INT NULL,
+  `season` INT NULL,
+  `start_time` INT NOT NULL,
+  `lobby_type` INT NULL,
+  `match_id` BIGINT(20) NOT NULL,
+  INDEX `fk_LobbyInfo_Match_idx` (`match_id` ASC) VISIBLE,
+  PRIMARY KEY (`match_id`),
   CONSTRAINT `fk_LobbyInfo_Match`
-    FOREIGN KEY (`matchID`)
-    REFERENCES `mydb`.`MatchTable` (`matchID`)
+    FOREIGN KEY (`match_id`)
+    REFERENCES `mydb`.`match_table` (`match_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Player Info`
+-- Table `mydb`.`player_info`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Player Info` (
-  `playerSlot` INT NOT NULL,
-  `isRadiant` TINYINT NOT NULL,
-  `item0` INT NULL,
-  `item1` INT NULL,
-  `item2` INT NULL,
-  `item3` INT NULL,
-  `item4` INT NULL,
-  `item5` INT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`player_info` (
+  `player_slot` INT NOT NULL,
+  `is_radiant` TINYINT NOT NULL,
+  `item_0` INT NULL,
+  `item_1` INT NULL,
+  `item_2` INT NULL,
+  `item_3` INT NULL,
+  `item_4` INT NULL,
+  `item_5` INT NULL,
   `kills` INT NOT NULL,
   `deaths` INT NOT NULL,
   `assists` INT NOT NULL,
   `leaverStatus` INT NOT NULL,
-  `lastHits` INT NOT NULL,
+  `last_hits` INT NOT NULL,
   `denies` INT NOT NULL,
-  `goldPerMinute` DOUBLE NOT NULL,
-  `xpPerMinute` DOUBLE NOT NULL,
-  `playerID` INT NOT NULL,
-  `matchID` INT NOT NULL,
-  `heroID` INT NOT NULL,
-  `backpackItem1` INT NULL,
-  `backpackItem2` INT NULL,
-  `backpackItem3` INT NULL,
-  `backpackItem4` INT NULL,
-  PRIMARY KEY (`playerID`, `matchID`),
-  INDEX `fk_Player Info_Match1_idx` (`matchID` ASC),
+  `gold_per_min` DOUBLE NOT NULL,
+  `xp_per_min` DOUBLE NOT NULL,
+  `player_id` INT NOT NULL,
+  `match_id` BIGINT(20) NOT NULL,
+  `hero_id` INT NOT NULL,
+  `backpack_1` INT NULL,
+  `backpack_2` INT NULL,
+  `backpack_3` INT NULL,
+  `backpack_4` INT NULL,
+  PRIMARY KEY (`player_id`, `match_id`),
+  INDEX `fk_Player Info_Match1_idx` (`match_id` ASC) VISIBLE,
   CONSTRAINT `fk_Player Info_Match1`
-    FOREIGN KEY (`matchID`)
-    REFERENCES `mydb`.`MatchTable` (`matchID`)
+    FOREIGN KEY (`match_id`)
+    REFERENCES `mydb`.`match_table` (`match_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Heros`
+-- Table `mydb`.`hero`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Heros` (
-  `heroID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`hero` (
+  `hero_id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`heroID`))
+  PRIMARY KEY (`hero_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`PickBan`
+-- Table `mydb`.`pick_ban`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PickBan` (
-  `matchID` INT NOT NULL,
-  INDEX `fk_PickBan_Match1_idx` (`matchID` ASC),
-  PRIMARY KEY (`matchID`),
+CREATE TABLE IF NOT EXISTS `mydb`.`pick_ban` (
+  `match_id` BIGINT(20) NOT NULL,
+  INDEX `fk_PickBan_Match1_idx` (`match_id` ASC) VISIBLE,
+  PRIMARY KEY (`match_id`),
   CONSTRAINT `fk_PickBan_Match1`
-    FOREIGN KEY (`matchID`)
-    REFERENCES `mydb`.`MatchTable` (`matchID`)
+    FOREIGN KEY (`match_id`)
+    REFERENCES `mydb`.`match_table` (`match_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`PickBanEntry`
+-- Table `mydb`.`pick_ban_entry`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PickBanEntry` (
-  `isPick` TINYINT NOT NULL,
-  `heroID` INT NOT NULL,
-  `isRadiant` TINYINT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`pick_ban_entry` (
+  `is_pick` TINYINT NOT NULL,
+  `hero_id` INT NOT NULL,
+  `is_radiant` TINYINT NOT NULL,
   `order` INT NOT NULL,
-  `matchID` INT NOT NULL,
-  PRIMARY KEY (`isPick`),
-  INDEX `fk_PickBanEntry_PickBan1_idx` (`matchID` ASC),
+  `match_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`is_pick`),
+  INDEX `fk_PickBanEntry_PickBan1_idx` (`match_id` ASC) VISIBLE,
   CONSTRAINT `fk_PickBanEntry_PickBan1`
-    FOREIGN KEY (`matchID`)
-    REFERENCES `mydb`.`PickBan` (`matchID`)
+    FOREIGN KEY (`match_id`)
+    REFERENCES `mydb`.`pick_ban` (`match_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`item` (
+  `item_id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `cost` INT NOT NULL,
+  `secret_shop` TINYINT NOT NULL,
+  `side_shop` TINYINT NOT NULL,
+  `recipe` TINYINT NOT NULL,
+  PRIMARY KEY (`item_id`))
 ENGINE = InnoDB;
 
 
