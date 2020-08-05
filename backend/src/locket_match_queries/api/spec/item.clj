89a03c5465fbf7218ee0bb42079d6b27061f3be6
Brian Brunner <brian.brunner.bb@gmail.com>
2020-08-05 02:27:34 -0600

    Fix more spec overflow issues

--- backend/src/locket_match_queries/api/spec/item.clj
+++ backend/src/locket_match_queries/api/spec/item.clj
@@ -1,9 +1,10 @@
 (ns locket-match-queries.api.spec.item
   (:require
-   [clojure.spec.alpha :as s]))
+   [clojure.spec.alpha :as s]
+   [locket-match-queries.api.spec.util :as util]))
 
 
-(s/def ::id int?)
+(s/def ::id (s/int-in 0 1000)); I have no idea, but probably enough?
 (s/def ::item_0 ::id)
 (s/def ::item_1 ::id)
 (s/def ::item_2 ::id)
@@ -16,11 +17,11 @@
 (s/def ::backpack_3 ::id)
 (s/def ::item_neutral ::id)
 
-(s/def ::cost int?)
-(s/def ::name int?)
-(s/def ::recipe int?)
-(s/def ::secret_shop int?)
-(s/def ::side_shop int?)
+(s/def ::cost ::util/uint16)
+(s/def ::name ::util/uint16)
+(s/def ::recipe ::util/uint16); TODO check these types
+(s/def ::secret_shop ::util/uint16)
+(s/def ::side_shop ::util/uint16)
 
 (s/def ::item
   (s/keys :req-un [::id ::cost ::name ::recipe ::secret_shop ::side_shop]))
