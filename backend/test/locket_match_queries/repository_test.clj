beca2ccb29fbe15d8b57b9367270a6d16de73497
Brian Brunner <brian.brunner.bb@gmail.com>
2020-11-09 14:50:56 -0700

    Hide auto inc column in snapshots
    unnamespace just the match snapshot so I can override the auto inc column.
    
    also fix auto reload bc kaocha uses a tools.namespace fork

--- backend/test/locket_match_queries/repository_test.clj
+++ backend/test/locket_match_queries/repository_test.clj
@@ -6,6 +6,7 @@
    [locket-match-queries.test.http-mocks :as http-mocks]
    [clojure.test :as t]
    [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as result-set]
    [locket-match-queries.api :as api]
    [honeysql.core :as sql]
    [snap.core :refer [match-snapshot]]
@@ -21,16 +22,32 @@
 
 (defn state-of
   [db & tables]
-  (into {}
-        (map (fn [k] [k
-                      (into #{}
-                            (jdbc/execute! db
-                                           (sql/format {:select [:*]
-                                                        :from [k]})))]))
-        tables))
+  (into
+    {}
+    (map (fn [k] [k
+                  (let [[table col] (if (seqable? k) k [k [:*]])]
+                    (into #{}
+                          (jdbc/execute! db
+                                         (sql/format {:select col
+                                                      :from [table]}))))]))
+    tables))
 
 (t/deftest testcontainer-working
   (t/is (:working? (jdbc/execute-one! *db* ["SELECT TRUE AS 'working?'"]))))
+
+(t/deftest check-clean-a
+  (t/is (empty? (:hero (state-of *db* :hero))))
+  (jdbc/execute-one! *db*
+                     (sql/format {:insert-into :hero
+                                  :values [{:hero_id 0 :name "a"}]}))
+  (t/is (= #{#:hero{:hero_id 0 :name "a"}} (:hero (state-of *db* :hero)))))
+
+(t/deftest check-clean-b
+  (t/is (empty? (:hero (state-of *db* :hero))))
+  (jdbc/execute-one! *db*
+                     (sql/format {:insert-into :hero
+                                  :values [{:hero_id 0 :name "b"}]}))
+  (t/is (= #{#:hero{:hero_id 0 :name "b"}} (:hero (state-of *db* :hero)))))
 
 (t/deftest can-populate-heroes
   (repo/populate-hero-table *db* (<!! (api/get-hero-data http-mocks/fake-key)))
@@ -49,4 +66,7 @@
       [sample.matches/with-buffs sample.matches/with-additional-units]))
   (match-snapshot
     ::matches
-    (state-of *db* :match-table :pick-ban-entry :player-info :additional-unit)))
+    (state-of (jdbc/with-options *db*
+                                 {:builder-fn result-set/as-unqualified-maps})
+              :match-table :pick-ban-entry
+              :player-info [:additional-unit [:* [nil :ai_differentiator]]])))
--- backend/test/locket_match_queries/test/dbowner.clj
+++ backend/test/locket_match_queries/test/dbowner.clj
@@ -1,13 +1,11 @@
 (ns locket-match-queries.test.dbowner
   "Hack to prevent db from getting reloaded by tools.namespace.
   This ns shouldn't depend on anything else, so it shouldn't ever be selected to reload on change."
-  (:require
-   [clojure.tools.namespace.repl :refer [disable-unload! disable-reload!]])
+  ;; kaocha uses a fork that checks a different key to skip reload
+  {:clojure.tools.namespace.repl/load false
+   :lambdaisland.tools.namespace.repl/load false}
   (:import
    (org.testcontainers.containers MySQLContainer)))
-
-(disable-unload!)
-(disable-reload!)
 
 ;; `latest-dev` is built from the working model in /db
 (def container-name "locket-ci-db:dev-latest")
